0f34a8d37dce306e0838ba2eeab5e3f571d39adc
Logicism <blazewalker462@protonmail.com>
2019-08-29 22:24:20 -0700

    Add Custom Help Menu

---
+++ src/me/Logicism/PabloBotRewrittenTestPlugin/TestPluginHelpMenu.java
@@ -0,0 +1,18 @@
+package me.Logicism.PabloBotRewrittenTestPlugin;
+
+import me.Logicism.PabloBotRewritten.api.plugin.PluginHelpMenu;
+import me.Logicism.PabloBotRewritten.utils.GuildUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+public class TestPluginHelpMenu extends PluginHelpMenu {
+
+    @Override
+    public void executeHelpMenu(Member member, MessageChannel channel) {
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle("TestPlugin Help Menu");
+        eb.addField("Commands to Test PabloBot's Functionality", GuildUtils.getGuildPrefix(((TextChannel) channel).getGuild().getId()) + "testerror - Test the ErrorManager\n" + GuildUtils.getGuildPrefix(((TextChannel) channel).getGuild().getId()) + "testconfig - Test the Configuration", true);
+    }
+}
