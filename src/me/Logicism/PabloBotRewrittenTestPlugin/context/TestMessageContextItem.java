2838854e8f28d49ce625382e9354aed3c95d6cb0
blazewalker462 <blazewalker462@protonmail.com>
2022-05-18 21:18:45 -0700

    Fix Context Menu Items

--- src/me/Logicism/PabloBotRewrittenTestPlugin/context/TestMessageContextItem.java
+++ src/me/Logicism/PabloBotRewrittenTestPlugin/context/TestMessageContextItem.java
@@ -3,14 +3,14 @@ package me.Logicism.PabloBotRewrittenTestPlugin.context;
 import me.Logicism.PabloBotRewritten.api.context.MessageContextAction;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 public class TestMessageContextItem implements MessageContextAction {
 
     @Override
-    public void execute(Message message, Member member, User user, MessageChannel channel, String s) {
-        channel.sendMessage("The messaged you executed this action to is `" + message.getContentRaw() + "` and you are " + user.getAsTag()).queue();
+    public void execute(Message message, Member member, User user, InteractionHook hook, String s) {
+        hook.sendMessage("The messaged you executed this action to is `" + message.getContentRaw() + "` and you are " + user.getAsTag()).queue();
     }
 
 }
--- src/me/Logicism/PabloBotRewrittenTestPlugin/context/TestUserContextItem.java
+++ src/me/Logicism/PabloBotRewrittenTestPlugin/context/TestUserContextItem.java
@@ -2,14 +2,14 @@ package me.Logicism.PabloBotRewrittenTestPlugin.context;
 
 import me.Logicism.PabloBotRewritten.api.context.UserContextAction;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 public class TestUserContextItem implements UserContextAction {
 
     @Override
-    public void execute(Member member, User user, Member memberAuthor, User userAuthor, MessageChannel channel, String s) {
-        channel.sendMessage("The user you executed this action to is " + user.getAsTag() + " and you are " + userAuthor.getAsTag()).queue();
+    public void execute(Member member, User user, Member memberAuthor, User userAuthor, InteractionHook hook, String s) {
+        hook.sendMessage("The user you executed this action to is " + user.getAsTag() + " and you are " + userAuthor.getAsTag()).queue();
     }
 
 }
