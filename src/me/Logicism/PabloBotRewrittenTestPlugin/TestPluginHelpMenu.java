f283da1242ca69914a342b2fdfb0f0a0914573e9
blazewalker462 <blazewalker462@protonmail.com>
2022-09-02 23:43:12 -0700

    Update to 1.7 with new updated help

--- src/me/Logicism/PabloBotRewrittenTestPlugin/TestPluginHelpMenu.java
+++ src/me/Logicism/PabloBotRewrittenTestPlugin/TestPluginHelpMenu.java
@@ -15,7 +15,8 @@ public class TestPluginHelpMenu extends PluginHelpMenu {
     public void executeHelpMenu(Member member, MessageChannel channel) {
         EmbedBuilder eb = new EmbedBuilder();
         eb.setAuthor("TestPlugin Help Menu");
-        eb.addField("Commands to Test Pablo!'s Functionality", GuildUtils.getGuildPrefix(((TextChannel) channel).getGuild().getId()) + "testerror - Test the ErrorManager\n" + GuildUtils.getGuildPrefix(((TextChannel) channel).getGuild().getId()) + "testconfig - Test the Configuration", true);
+        eb.setDescription("Pablo!'s Prefix Commands are now deprecated! Please use Slash Commands to execute commands!");
+        eb.addField("Commands to Test Pablo!'s Functionality", "/testbutton - Test the Buttons Functionality\n" + "/testconfig - Test the Configuration" + "/testephemeral - Test the Ephemeral Message Functionality\n" + "/testerror - Test the ErrorManager\n" + "/testmultiselectmenu - Test the Multi Select Menu Functionality\n" + "/testselectmenu - Test the Select Menu Functionality\n" + "/testslash - Test the Slash Commands Functionality\n" + "/testsubcommands - Test the SubCommands Functionality\n", true);
         eb.setFooter("Custom Help Menu Test");
         channel.sendMessageEmbeds(eb.build()).queue();
     }
@@ -24,7 +25,7 @@ public class TestPluginHelpMenu extends PluginHelpMenu {
     public void executeHelpMenu(Member member, InteractionHook hook) {
         EmbedBuilder eb = new EmbedBuilder();
         eb.setAuthor("TestPlugin Help Menu");
-        eb.addField("Commands to Test Pablo!'s Functionality", "/testerror - Test the ErrorManager\n" + "/testconfig - Test the Configuration", true);
+        eb.addField("Commands to Test Pablo!'s Functionality", "/testbutton - Test the Buttons Functionality\n" + "/testconfig - Test the Configuration" + "/testephemeral - Test the Ephemeral Message Functionality\n" + "/testerror - Test the ErrorManager\n" + "/testmultiselectmenu - Test the Multi Select Menu Functionality\n" + "/testselectmenu - Test the Select Menu Functionality\n" + "/testslash - Test the Slash Commands Functionality\n" + "/testsubcommands - Test the SubCommands Functionality\n", true);
         eb.setFooter("Custom Help Menu Test");
         hook.sendMessageEmbeds(eb.build()).queue();
     }
@@ -33,7 +34,7 @@ public class TestPluginHelpMenu extends PluginHelpMenu {
     public void executeHelpMenu(Member member, SelectMenuInteractionEvent e) {
         EmbedBuilder eb = new EmbedBuilder();
         eb.setAuthor("TestPlugin Help Menu");
-        eb.addField("Commands to Test Pablo!'s Functionality", "/testerror - Test the ErrorManager\n" + "/testconfig - Test the Configuration", true);
+        eb.addField("Commands to Test Pablo!'s Functionality", "/testbutton - Test the Buttons Functionality\n" + "/testconfig - Test the Configuration" + "/testephemeral - Test the Ephemeral Message Functionality\n" + "/testerror - Test the ErrorManager\n" + "/testmultiselectmenu - Test the Multi Select Menu Functionality\n" + "/testselectmenu - Test the Select Menu Functionality\n" + "/testslash - Test the Slash Commands Functionality\n" + "/testsubcommands - Test the SubCommands Functionality\n", true);
         eb.setFooter("Custom Help Menu Test");
         e.editMessageEmbeds(eb.build()).queue();
     }
